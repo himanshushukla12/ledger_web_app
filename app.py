6ca071aff7d4de4d25b087468c37a22de1b1e01e
Himanshu Shukla <43571898+himanshushukla12@users.noreply.github.com>
2023-10-17 09:32:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,27 @@
-
 from flask import Flask, render_template, request, redirect, url_for, flash
+from web3 import Web3
+from Crypto.Cipher import AES
+
 
 app = Flask(__name__)
 app.secret_key = "some_secret_key"  # used for flashing messages
 
-# Set up connection with Ethereum testnet
-# For the sake of this example, we're just mocking the web3 connection
-# In a real scenario, you'd connect to an Ethereum node (e.g., via Infura)
-
-class MockWeb3:
-    def __init__(self):
+# Mock Ethereum connection
+class MockWeb3(Web3):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self._entries = []
 
-    @property
-    def eth(self):
-        return self
-
-    def contract(self, **kwargs):
-        return self
-
-    def functions(self):
-        return self
-
     def addEntry(self, description, amount):
-        # Now actually store the entry in the mock class
         self._entries.append((description, amount))
+        return True
 
     def getEntryCount(self):
-        # Return the count of stored entries
         return len(self._entries)
 
     def getEntry(self, index):
-        # Return the entry at the specified index
         return self._entries[index]
+
 web3 = MockWeb3()
 
 @app.route('/', methods=['GET', 'POST'])
@@ -40,14 +29,17 @@ def index():
     if request.method == 'POST':
         description = request.form.get('description')
         amount = int(request.form.get('amount'))
-        # Add entry to the smart contract
-        web3.eth.contract().functions().addEntry(description, amount)
-        flash('Entry added successfully!', 'success')
+        # Add entry to the mock Ethereum network
+        success = web3.addEntry(description, amount)
+        if success:
+            flash('Entry added successfully!', 'success')
+        else:
+            flash('Failed to add entry!', 'danger')
         return redirect(url_for('index'))
     
     # Retrieve all entries
-    entry_count = web3.eth.contract().functions().getEntryCount()
-    entries = [web3.eth.contract().functions().getEntry(i) for i in range(entry_count)]
+    entry_count = web3.getEntryCount()
+    entries = [web3.getEntry(i) for i in range(entry_count)]
     
     return render_template('index.html', entries=entries)
 
@@ -55,9 +47,12 @@ def index():
 def add():
     description = request.form.get('description')
     amount = int(request.form.get('amount'))
-    # Add entry to the smart contract
-    web3.eth.contract().functions().addEntry(description, amount)
-    flash('Entry added successfully!', 'success')
+    # Add entry to the mock Ethereum network
+    success = web3.addEntry(description, amount)
+    if success:
+        flash('Entry added successfully!', 'success')
+    else:
+        flash('Failed to add entry!', 'danger')
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
